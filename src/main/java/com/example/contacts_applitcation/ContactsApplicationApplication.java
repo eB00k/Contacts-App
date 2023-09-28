9b2511460365e4673ca1e75d9e83393610249af0
Dastan Akatov <107542637+eB00k@users.noreply.github.com>
2023-09-28 14:24:24 +0600

    start project

---
+++ src/main/java/com/example/contacts_applitcation/ContactsApplicationApplication.java
@@ -0,0 +1,12 @@
+package com.example.contacts_applitcation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsApplicationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsApplicationApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/contacts_applitcation/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.contacts_applitcation.service.impl;
+
+import com.example.contacts_applitcation.io.UserRepository;
+import com.example.contacts_applitcation.io.entity.UserEntity;
+import com.example.contacts_applitcation.service.UserService;
+import com.example.contacts_applitcation.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        UserEntity storedUserDetails = userRepository.findByEmail(userDto.getEmail());
+        if (storedUserDetails != null) {
+            throw new RuntimeException("Record already exist");
+        }
+
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(userDto,userEntity);
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("testUserId");
+
+        UserEntity storedUser = userRepository.save(userEntity);
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedUser,returnValue);
+        return returnValue;
+    }
+
+}
